398536dacfffc0531055fb3308f03b6cbe17b4e9
allenwu <allenzhiwei0@gmail.com>
2024-10-25 17:36:48 +0800

    comment CrossOrigin

--- src/main/java/com/nusiss/userservice/controller/AddressController.java
+++ src/main/java/com/nusiss/userservice/controller/AddressController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5000")
+//@CrossOrigin(origins = "http://localhost:5000")
 @RestController
 @RequestMapping("/api/addresses")
 public class AddressController {
