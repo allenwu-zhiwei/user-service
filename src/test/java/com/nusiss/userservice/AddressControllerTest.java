dee5f873c7a6791a07e4e819b211cbce2594ae97
allenwu <allenzhiwei0@gmail.com>
2024-11-17 11:51:56 +0800

    comment springboottest

--- src/test/java/com/nusiss/userservice/AddressControllerTest.java
+++ src/test/java/com/nusiss/userservice/AddressControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+//@SpringBootTest
 @AutoConfigureMockMvc
 public class AddressControllerTest {
 
